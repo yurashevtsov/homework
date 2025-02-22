ba2dc362212961655d3527ca5f37e36651a32fdc
yurashevtsov <yura.shevtsov92@gmail.com>
2025-02-22 15:29:52 +0300

    init setup for migrations

---
+++ src/database/config/database.js
@@ -0,0 +1,41 @@
+// const fs = require("fs");
+const appConfig = require("@src/config/index");
+
+module.exports = {
+  development: {
+    username: appConfig.dbUser,
+    password: appConfig.dbPassword,
+    database: appConfig.dbName,
+    host: appConfig.dbHost,
+    port: appConfig.dbPort,
+    dialect: appConfig.dbDialect,
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  test: {
+    username: appConfig.dbUser,
+    password: appConfig.dbPassword,
+    database: appConfig.dbName,
+    host: appConfig.dbHost,
+    port: appConfig.dbPort,
+    dialect: appConfig.dbDialect,
+    dialectOptions: {
+      bigNumberStrings: true,
+    },
+  },
+  // production: {
+  //   username: process.env.PROD_DB_USERNAME,
+  //   password: process.env.PROD_DB_PASSWORD,
+  //   database: process.env.PROD_DB_NAME,
+  //   host: process.env.PROD_DB_HOSTNAME,
+  //   port: process.env.PROD_DB_PORT,
+  //   dialect: "mysql",
+  //   dialectOptions: {
+  //     bigNumberStrings: true,
+  //     ssl: {
+  //       ca: fs.readFileSync(__dirname + "/mysql-ca-main.crt"),
+  //     },
+  //   },
+  // },
+};
