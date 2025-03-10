45f102f73ccf8493bd047c1734469187ca02869d
Yura Shevtsov <yura.shevtsov92@gmail.com>
2025-03-11 00:01:05 +0300

    finished tests for posts endpoints

--- src/__tests__/postsEndpoints/postsPutEndpoints.int.test.js
+++ src/__tests__/postsEndpoints/postsPutEndpoints.int.test.js
@@ -42,15 +42,108 @@ describe(`PUT ${POSTS_ENDPOINT}`, () => {
     await closeDB();
   });
 
+  afterEach(async () => {
+    // clearing table/tags tables after each test
+    await Promise.all([clearPostTable(), clearTagTable()]);
+  });
+
   test("should update post data", async () => {
+    const updatePostData = {
+      title: "updated title",
+      content: "updated content",
+    };
     // create post that we can update
-    const createRes = await createPostsWithTags([
+    const createdPost = await createPostsWithTags([
+      { userId: auhtorizedUser.id, ...POST_TO_CREATE },
+    ]);
+    // make sure that post was created
+    const requiredFields = ["id", "userId", "title", "content", "tags"];
+
+    requiredFields.forEach((field) => {
+      expect(createdPost[0]).toHaveProperty(field);
+    });
+
+    // request to update created post
+    const updateRes = await request(app)
+      .put(`${POSTS_ENDPOINT}${createdPost[0].id}`)
+      .set("Authorization", `Bearer ${authToken}`)
+      .send({
+        title: updatePostData.title,
+        content: updatePostData.content,
+      });
+
+    // console.log(updateRes.text);
+    expect(updateRes.status).toBe(200);
+    expect(updateRes.body.title).toBe(updatePostData.title);
+    expect(updateRes.body.content).toBe(updatePostData.content);
+  });
+
+  test("should update tags data", async () => {
+    const tagsToUpdate = "yolo, fun";
+    // create post that we can update
+    const createdPost = await createPostsWithTags([
+      { userId: auhtorizedUser.id, ...POST_TO_CREATE },
+    ]);
+    // make sure that post was created
+    const requiredFields = ["id", "userId", "title", "content", "tags"];
+
+    requiredFields.forEach((field) => {
+      expect(createdPost[0]).toHaveProperty(field);
+    });
+
+    // request to update created post
+    const updateRes = await request(app)
+      .put(`${POSTS_ENDPOINT}${createdPost[0].id}`)
+      .set("Authorization", `Bearer ${authToken}`)
+      .send({
+        tags: tagsToUpdate,
+      });
+
+    // `updatedRes.tags` wil look like this  [ { id: 119, name: 'yolo' }, { id: 120, name: 'fun' } ]
+    // making it ["yolo", "fun"];
+    const updatedTagNames = updateRes.body.tags.map((tag) => tag.name);
+    // returns true if all updated tag names are included in request
+    const correctlyUpdatedTags = updatedTagNames.every((tagName) =>
+      tagsToUpdate.includes(tagName)
+    );
+
+    expect(updateRes.status).toBe(200);
+    expect(correctlyUpdatedTags).toBe(true);
+  });
+
+  test("should not update post that doesnt belong to him (404)", async () => {
+    let secondUser;
+    let secondUserAuthToken;
+    // create a post with tags by created user (before tests)
+    const firstUserPost = await createPostsWithTags([
       { userId: auhtorizedUser.id, ...POST_TO_CREATE },
     ]);
 
+    expect(firstUserPost[0].id).toBeDefined();
+
+    // create a new user
+    const signupRes = await request(app).post(SIGNUP_ENDPOINT).send({
+      username: "seconduser",
+      email: "seconduser@mail.com",
+      password: "pass1234",
+      repeatPassword: "pass1234",
+    });
+
+    secondUser = signupRes.body.user;
+    secondUserAuthToken = signupRes.body.token;
+    // token should be present
+    expect(signupRes.body.token).toBeDefined();
+    // use new token to edit post created by first user
+    const updateRes = await request(app)
+      .put(`${POSTS_ENDPOINT}${firstUserPost[0].id}`)
+      .set("Authorization", `Bearer ${secondUserAuthToken}`)
+      .send({
+        title: "Yoooooooo",
+      });
+
+    // console.log(updateRes.text);
+    // get 404 error
+    expect(updateRes.status).toBe(404);
+    expect(updateRes.text).toContain(`not found`);
   });
-
-  test("should update tags data", async () => {});
-
-  test("should not update post that doesnt belong to him", async () => {});
 });
