41cca5dcc2668fa2fe5d1c1ed4705ab86c4e5cc6
yurashevtsov <yura.shevtsov92@gmail.com>
2025-03-12 15:22:11 +0300

    finised tests for  tags put endpoints

--- src/__tests__/tagsEndpoints/tagsPutEndpoints.int.test.js
+++ src/__tests__/tagsEndpoints/tagsPutEndpoints.int.test.js
@@ -39,7 +39,52 @@ describe(`GET tags endpoints`, () => {
     await closeDB();
   });
 
-  describe("", () => {
-    test("should", async () => {});
+  // ! finish tests
+  describe(`PUT ${TAGS_ENDPOINT}:id`, () => {
+    test("should update tag by id", async () => {
+      const updatedTagName = { name: "updated name" };
+      const expectedTagName = "updated name";
+      // create tag
+      const [createdTag] = await createTags("news");
+
+      expect(createdTag).toHaveProperty("id");
+      expect(createdTag.name).toBe("news");
+      // update tag
+      const updateRes = await request(app)
+        .put(`${TAGS_ENDPOINT}${createdTag.id}`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send(updatedTagName);
+
+      console.log(updateRes.text);
+      expect(updateRes.status).toBe(200);
+      expect(updateRes.body.name).toBe(expectedTagName);
+    });
+
+    test("should throw an error if tag isnt found 404", async () => {
+      const tagId = 9999999;
+      const updatedTagName = { name: "updated name" };
+
+      const updateRes = await request(app)
+        .put(`${TAGS_ENDPOINT}${tagId}`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send(updatedTagName);
+
+      expect(updateRes.status).toBe(404);
+      expect(updateRes.text).toContain("not found");
+    });
+
+    test("should throw an error on inval tag id", async () => {
+      const tagId = "asd";
+      const updatedTagName = { name: "updated name" };
+
+      const updateRes = await request(app)
+        .put(`${TAGS_ENDPOINT}${tagId}`)
+        .set("Authorization", `Bearer ${authToken}`)
+        .send(updatedTagName);
+
+      // console.log(updateRes.text);
+      expect(updateRes.status).toBe(400);
+      expect(updateRes.text).toContain('"id" must be a number');
+    });
   });
 });
